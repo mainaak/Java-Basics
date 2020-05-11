38a9e5ea0951ccd08491c1b18034c8286442bc20
mainaak <mainaakaanand@gmail.com>
2020-05-12 00:21:29 +0530

    add all

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import javaIntermediate.StringMethods;
 public class Main {
 
     public static void main(String args[]) {
-        new Recursion();
+        new StringMethods("a","a");
     }
 }
--- src/javaIntermediate/StringMethods.java
+++ src/javaIntermediate/StringMethods.java
@@ -1,56 +1,45 @@
 package javaIntermediate;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class StringMethods {
-    public StringMethods(String starting, String ending){
+
+    public StringMethods(String startingPattern, String endingPattern) {
+        ArrayList<String> c = endingWith(beginningWith(takeInputFromUser(), startingPattern), endingPattern);
+        for (String d : c) {
+            System.out.println(d);
+        }
+    }
+
+    private ArrayList<String> takeInputFromUser() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How many strings do you wish to enter?");
-        String[] strings = new String[Integer.parseInt(scanner.nextLine())];
-        Integer counter = 0;
-        for (String s : strings){
-            System.out.println("Enter string "+ (counter+1));
-            strings[counter] = scanner.nextLine();
-            counter++;
+        int valueCounter = 0;
+        ArrayList<String> inputArray = new ArrayList<>();
+
+        while (valueCounter < 5) {
+            System.out.println("Enter value " + ++valueCounter);
+            inputArray.add(scanner.nextLine());
         }
-
-        String[] a = startsWith(strings,starting);
-
-        String[] filteredString = endsWith(a,ending);
-
-        for (String f : filteredString ){
-            System.out.println(f);
-        }
+        return inputArray;
     }
 
-    private String[] startsWith(String[] startsWith, String starting){
-        String[] useItForStart = new String[startsWith.length];
-        Integer counter = 0;
-        for (String b : startsWith){
-            if (b.startsWith(starting)){
-                useItForStart[counter++] = b;
+    public ArrayList<String> beginningWith(ArrayList<String> unfilteredList, String startingPattern) {
+        ArrayList<String> finalList = new ArrayList<>();
+        for (String validateItem : unfilteredList) {
+            if (validateItem.startsWith(startingPattern)) {
+                finalList.add(validateItem);
             }
         }
-        String[] noNull = new String[counter];
-        counter = 0;
-        for (String nn : useItForStart){
-            if (nn!=null) {
-                noNull[counter++] = nn;
-            }
-        }
-
-        return noNull;
+        return finalList;
     }
 
-    private String[] endsWith(String[] endsWith, String ending){
-        String[] useItForEnd = new String[endsWith.length];
-        Integer counter = 0;
-        for (String b : endsWith){
-
-            if (b.endsWith(ending)){
-                useItForEnd[counter++] = b;
+    private ArrayList<String> endingWith(ArrayList<String> unfilteredList, String endingPattern) {
+        ArrayList<String> finalList = new ArrayList<>();
+        for (String validateItem : unfilteredList) {
+            if (validateItem.endsWith(endingPattern)) {
+                finalList.add(validateItem);
             }
         }
-        return useItForEnd;
+        return finalList;
     }
 }
